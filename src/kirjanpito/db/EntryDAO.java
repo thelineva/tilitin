adf8c883babd3fc37bd5ba8c153f9e36b1afff33
Tommi Helineva <tommi.helineva@gmail.com>
2011-02-16 12:26:38 +0200

    Viennit voidaan hakea tietokannasta myös aikajärjestyksessä

--- src/kirjanpito/db/EntryDAO.java
+++ src/kirjanpito/db/EntryDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
  * <code>EntryDAO</code>:n avulla voidaan lisätä, muokata ja
  * poistaa vientejä sekä hakea olemassa olevien vientien
  * tietoja.
- * 
+ *
  * @author Tommi Helineva
  */
 public interface EntryDAO {
@@ -15,61 +15,71 @@ public interface EntryDAO {
 	 * Järjestetään tositenumeron mukaan.
 	 */
 	public static final int ORDER_BY_DOCUMENT_NUMBER = 1;
-	
+
 	/**
-	 * Järjestetään tilinumeron mukaan.
+	 * Järjestetään päivämäärän mukaan.
 	 */
-	public static final int ORDER_BY_ACCOUNT_NUMBER = 2;
-	
+	public static final int ORDER_BY_DOCUMENT_DATE = 2;
+
+	/**
+	 * Järjestetään tilinumeron ja tositenumeron mukaan.
+	 */
+	public static final int ORDER_BY_ACCOUNT_NUMBER_AND_DOCUMENT_NUMBER = 3;
+
+	/**
+	 * Järjestetään tilinumeron ja päivämäärän mukaan.
+	 */
+	public static final int ORDER_BY_ACCOUNT_NUMBER_AND_DOCUMENT_DATE = 4;
+
 	/**
 	 * Tallentaa viennin tiedot tietokantaan.
-	 * 
+	 *
 	 * @param entry tallennettava vienti
 	 * @throws DataAccessException jos tallentaminen epäonnistuu
 	 */
 	public void save(Entry entry) throws DataAccessException;
-	
+
 	/**
 	 * Poistaa viennin tiedot tietokannasta.
-	 * 
+	 *
 	 * @param entryId poistettavan viennin tunniste
 	 * @throws DataAccessException jos poistaminen epäonnistuu
 	 */
 	public void delete(int entryId) throws DataAccessException;
-	
+
 	/**
 	 * Poistaa tietyn tilikauden kaikki viennit tietokannasta.
-	 * 
+	 *
 	 * @param periodId tilikauden tunniste
 	 * @throws DataAccessException jos poistaminen epäonnistuu
 	 */
 	public void deleteByPeriodId(int periodId) throws DataAccessException;
-	
+
 	/**
 	 * Hakee tietokannasta tiettyyn tositteeseen kuuluvat
 	 * viennit.
-	 * 
+	 *
 	 * @param documentId tositteen tunniste
 	 * @return viennit
 	 * @throws DataAccessException jos tietojen hakeminen epäonnistuu
 	 */
 	public List<Entry> getByDocumentId(int documentId)
 		throws DataAccessException;
-	
+
 	/**
 	 * Hakee tietokannasta tiettyihin tositteisiin kuuluvat
 	 * viennit.
-	 * 
+	 *
 	 * @param documentIds tositteiden tunnisteet
 	 * @param callback callback
 	 * @throws DataAccessException jos tietojen hakeminen epäonnistuu
 	 */
 	public void getByDocuments(List<Document> documents, DTOCallback<Entry> callback)
 		throws DataAccessException;
-	
+
 	/**
 	 * Hakee tietokannasta kaikki tietyn tilikauden viennit.
-	 * 
+	 *
 	 * @param periodId tilikauden tunniste
 	 * @param orderBy <code>ORDER_BY_DOCUMENT_NUMBER</code> tai
 	 * <code>ORDER_BY_ACCOUNT_NUMBER</code>
@@ -78,11 +88,11 @@ public interface EntryDAO {
 	 */
 	public void getByPeriodId(int periodId, int orderBy, DTOCallback<Entry> callback)
 		throws DataAccessException;
-	
+
 	/**
 	 * Hakee tietokannasta tietyn tilikauden viennit, jotka
 	 * kohdistuvat tiettyyn tiliin. Viennit järjestetään päivämäärän mukaan.
-	 * 
+	 *
 	 * @param periodId tilikauden tunniste
 	 * @param accountId tilin tunniste
 	 * @param callback callback
@@ -90,11 +100,11 @@ public interface EntryDAO {
 	 */
 	public void getByPeriodIdAndAccountId(int periodId,
 			int accountId, DTOCallback<Entry> callback) throws DataAccessException;
-	
+
 	/**
 	 * Hakee tietokannasta tietyn tilikauden viennit tietyltä
 	 * aikaväliltä.
-	 * 
+	 *
 	 * @param periodId tilikauden tunniste
 	 * @param startDate alkamispäivämäärä
 	 * @param endDate päättymispäivämäärä
@@ -104,12 +114,12 @@ public interface EntryDAO {
 	public void getByPeriodIdAndDate(int periodId,
 			Date startDate, Date endDate, DTOCallback<Entry> callback)
 			throws DataAccessException;
-	
+
 	/**
 	 * Hakee tietokannasta viennit tositenumeroväliltä
 	 * <code>startNumber</code>..<code>endNumber</code>
 	 * tietyltä tilikaudelta.
-	 * 
+	 *
 	 * @param periodId tilikauden tunniste
 	 * @param startNumber tositenumerovälin alku
 	 * @param endNumber tositenumerovälin loppu
--- src/kirjanpito/db/sql/SQLEntryDAO.java
+++ src/kirjanpito/db/sql/SQLEntryDAO.java
@@ -18,38 +18,38 @@ import kirjanpito.db.EntryDAO;
  * poistaa vientejä sekä hakea olemassa olevien vientien
  * tietoja. Aliluokassa on määriteltävä toteutukset metodeilla, jotka
  * palauttavat SQL-kyselymerkkijonot.
- * 
+ *
  * @author Tommi Helineva
  */
 public abstract class SQLEntryDAO implements EntryDAO {
 	public List<Entry> getByDocumentId(int documentId) throws DataAccessException {
 		ArrayList<Entry> list;
 		ResultSet rs;
-		
+
 		try {
 			PreparedStatement stmt = getSelectByDocumentIdQuery();
 			stmt.setInt(1, documentId);
 			rs = stmt.executeQuery();
 			list = new ArrayList<Entry>();
-			
+
 			while (rs.next()) {
 				list.add(createObject(rs));
 			}
-			
+
 			rs.close();
 			stmt.close();
 		}
 		catch (SQLException e) {
 			throw new DataAccessException(e.getMessage(), e);
 		}
-		
+
 		return list;
 	}
-	
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan tiettyyn tositteeseen
 	 * kuuluvat viennit.
-	 * 
+	 *
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
@@ -58,25 +58,25 @@ public abstract class SQLEntryDAO implements EntryDAO {
 
 	public void getByDocuments(List<Document> documents, DTOCallback<Entry> callback)
 		throws DataAccessException {
-		
+
 		ResultSet rs;
 		StringBuilder sb = new StringBuilder();
-		
+
 		for (Document document : documents) {
 			if (sb.length() > 0) sb.append(',');
 			sb.append(document.getId());
 		}
-		
+
 		try {
 			PreparedStatement stmt = getSelectByDocumentIdsQuery(
 					sb.toString());
-			
+
 			rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				callback.process(createObject(rs));
 			}
-			
+
 			rs.close();
 			stmt.close();
 		}
@@ -84,38 +84,42 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan tiettyihin tositteisiin
 	 * kuuluvat viennit.
-	 * 
+	 *
 	 * @param documentIds lista tositteiden tunnisteista
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getSelectByDocumentIdsQuery(
 		String documentIds) throws SQLException;
-	
+
 	public void getByPeriodId(int periodId, int orderBy,
 			DTOCallback<Entry> callback) throws DataAccessException
 	{
 		ResultSet rs;
-		
+
 		try {
 			PreparedStatement stmt = null;
-			
+
 			if (orderBy == ORDER_BY_DOCUMENT_NUMBER)
-				stmt = getSelectByPeriodIdOrderByDocumentQuery();
-			else if (orderBy == ORDER_BY_ACCOUNT_NUMBER)
-				stmt = getSelectByPeriodIdOrderByAccountQuery();
-			
+				stmt = getSelectByPeriodIdOrderByNumberQuery();
+			else if (orderBy == ORDER_BY_DOCUMENT_DATE)
+				stmt = getSelectByPeriodIdOrderByDateQuery();
+			else if (orderBy == ORDER_BY_ACCOUNT_NUMBER_AND_DOCUMENT_NUMBER)
+				stmt = getSelectByPeriodIdOrderByAccountAndNumberQuery();
+			else if (orderBy == ORDER_BY_ACCOUNT_NUMBER_AND_DOCUMENT_DATE)
+				stmt = getSelectByPeriodIdOrderByAccountAndDateQuery();
+
 			stmt.setInt(1, periodId);
 			rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				callback.process(createObject(rs));
 			}
-			
+
 			rs.close();
 			stmt.close();
 		}
@@ -123,33 +127,51 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan kaikki tietyn
 	 * tilikauden viennit. Rivit järjestetään tositenumeron mukaan.
-	 * 
+	 *
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
-	protected abstract PreparedStatement getSelectByPeriodIdOrderByDocumentQuery() throws SQLException;
-	
+	protected abstract PreparedStatement getSelectByPeriodIdOrderByNumberQuery() throws SQLException;
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan kaikki tietyn
-	 * tilikauden viennit. Rivit järjestetään tilinumeron mukaan.
-	 * 
+	 * tilikauden viennit. Rivit järjestetään päivämäärän mukaan.
+	 *
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
-	protected abstract PreparedStatement getSelectByPeriodIdOrderByAccountQuery() throws SQLException;
-	
+	protected abstract PreparedStatement getSelectByPeriodIdOrderByDateQuery() throws SQLException;
+
+	/**
+	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan kaikki tietyn
+	 * tilikauden viennit. Rivit järjestetään tilinumeron ja tositenumeron mukaan.
+	 *
+	 * @return SELECT-kysely
+	 * @throws SQLException jos kyselyn luominen epäonnistuu
+	 */
+	protected abstract PreparedStatement getSelectByPeriodIdOrderByAccountAndNumberQuery() throws SQLException;
+
+	/**
+	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan kaikki tietyn
+	 * tilikauden viennit. Rivit järjestetään tilinumeron ja päivämäärän mukaan.
+	 *
+	 * @return SELECT-kysely
+	 * @throws SQLException jos kyselyn luominen epäonnistuu
+	 */
+	protected abstract PreparedStatement getSelectByPeriodIdOrderByAccountAndDateQuery() throws SQLException;
+
 	public void getByPeriodIdAndAccountId(int periodId, int accountId,
 			DTOCallback<Entry> callback) throws DataAccessException
 	{
 		ResultSet rs;
-		
+
 		try {
 			PreparedStatement stmt;
-			
+
 			if (periodId < 0) {
 				stmt = getSelectByAccountIdQuery();
 				stmt.setInt(1, accountId);
@@ -159,13 +181,13 @@ public abstract class SQLEntryDAO implements EntryDAO {
 				stmt.setInt(1, periodId);
 				stmt.setInt(2, accountId);
 			}
-			
+
 			rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				callback.process(createObject(rs));
 			}
-			
+
 			rs.close();
 			stmt.close();
 		}
@@ -173,41 +195,41 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan kaikkien tilikausien
 	 * viennit, jotka kohdistuvat tiettyyn tiliin.
-	 * 
+	 *
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getSelectByAccountIdQuery() throws SQLException;
-	
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan tietyn tilikauden
 	 * viennit, jotka kohdistuvat tiettyyn tiliin.
-	 * 
+	 *
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getSelectByPeriodIdAndAccountIdQuery() throws SQLException;
-	
+
 	public void getByPeriodIdAndDate(int periodId, Date startDate,
 			Date endDate, DTOCallback<Entry> callback) throws DataAccessException {
-		
+
 		ResultSet rs;
-		
+
 		try {
 			PreparedStatement stmt = getSelectByPeriodIdAndDateQuery();
 			stmt.setInt(1, periodId);
 			stmt.setTimestamp(2, new java.sql.Timestamp(startDate.getTime()));
 			stmt.setTimestamp(3, new java.sql.Timestamp(endDate.getTime()));
 			rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				callback.process(createObject(rs));
 			}
-			
+
 			rs.close();
 			stmt.close();
 		}
@@ -215,13 +237,13 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan tietyn tilikauden
 	 * viennit tietyltä aikaväliltä. Kyselyssä on kolme parametria,
 	 * ensimmäinen tilikauden tunniste, toinen alkamispäivämäärä ja
 	 * kolmas päättymispäivämäärä.
-	 * 
+	 *
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
@@ -232,18 +254,18 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throws DataAccessException
 	{
 		ResultSet rs;
-		
+
 		try {
 			PreparedStatement stmt = getSelectByPeriodIdAndNumberQuery();
 			stmt.setInt(1, periodId);
 			stmt.setInt(2, startNumber);
 			stmt.setInt(3, endNumber);
 			rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				callback.process(createObject(rs));
 			}
-			
+
 			rs.close();
 			stmt.close();
 		}
@@ -251,19 +273,19 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Palauttaa SELECT-kyselyn, jonka avulla haetaan viennit
 	 * tietyltä tositenumeroväliltä ja tietyltä tilikaudelta.
-	 * 
+	 *
 	 * @return SELECT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getSelectByPeriodIdAndNumberQuery() throws SQLException;
-	
+
 	/**
 	 * Tallentaa viennin tiedot tietokantaan.
-	 * 
+	 *
 	 * @param obj tallennettava vienti
 	 * @throws DataAccessException jos tallentaminen epäonnistuu
 	 */
@@ -280,10 +302,10 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Lisää viennin tiedot tietokantaan.
-	 * 
+	 *
 	 * @param obj tallennettava vienti
 	 * @throws SQLException jos tallentaminen epäonnistuu
 	 */
@@ -293,18 +315,18 @@ public abstract class SQLEntryDAO implements EntryDAO {
 		stmt.executeUpdate();
 		stmt.close();
 	}
-	
+
 	/**
 	 * Palauttaa INSERT-kyselyn, jonka avulla rivi lisätään.
-	 * 
+	 *
 	 * @return INSERT-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getInsertQuery() throws SQLException;
-	
+
 	/**
 	 * Päivittää viennin tiedot tietokantaan.
-	 * 
+	 *
 	 * @param obj tallennettava tosite
 	 * @throws SQLException jos kyselyn suorittaminen epäonnistuu
 	 */
@@ -315,18 +337,18 @@ public abstract class SQLEntryDAO implements EntryDAO {
 		stmt.executeUpdate();
 		stmt.close();
 	}
-	
+
 	/**
 	 * Palauttaa UPDATE-kyselyn, jonka avulla rivin kaikki kentät päivitetään.
-	 * 
+	 *
 	 * @return UPDATE-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getUpdateQuery() throws SQLException;
-	
+
 	/**
 	 * Poistaa viennin tiedot tietokannasta.
-	 * 
+	 *
 	 * @param entryId poistettavan viennin tunniste
 	 * @throws DataAccessException jos poistaminen epäonnistuu
 	 */
@@ -341,19 +363,19 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Palauttaa DELETE-kyselyn, jonka avulla poistetaan rivi. Kyselyssä
 	 * on yksi parametri, joka on viennin tunniste.
-	 * 
+	 *
 	 * @return DELETE-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getDeleteQuery() throws SQLException;
-	
+
 	/**
 	 * Poistaa tietyn tilikauden kaikki viennit tietokannasta.
-	 * 
+	 *
 	 * @param periodId tilikauden tunniste
 	 * @throws DataAccessException jos poistaminen epäonnistuu
 	 */
@@ -368,20 +390,20 @@ public abstract class SQLEntryDAO implements EntryDAO {
 			throw new DataAccessException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Palauttaa DELETE-kyselyn, jonka avulla poistetaan kaikki
 	 * tietyn tilikauden viennit.
-	 * 
+	 *
 	 * @return DELETE-kysely
 	 * @throws SQLException jos kyselyn luominen epäonnistuu
 	 */
 	protected abstract PreparedStatement getDeleteByPeriodIdQuery() throws SQLException;
-	
+
 	/**
 	 * Lukee <code>ResultSetistä</code> rivin kentät ja
 	 * luo <code>Entry</code>-olion.
-	 * 
+	 *
 	 * @param rs <code>ResultSet</code>-olio, josta kentät luetaan.
 	 * @return luotu olio
 	 * @throws SQLException jos kenttien lukeminen epäonnistuu
@@ -397,10 +419,10 @@ public abstract class SQLEntryDAO implements EntryDAO {
 		obj.setRowNumber(rs.getInt(7));
 		return obj;
 	}
-	
+
 	/**
 	 * Asettaa valmistellun kyselyn parametreiksi olion tiedot.
-	 * 
+	 *
 	 * @param stmt kysely, johon tiedot asetetaan
 	 * @param obj olio, josta tiedot luetaan
 	 * @throws SQLException jos tietojen asettaminen epäonnistuu
