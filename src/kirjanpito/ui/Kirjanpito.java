5997f3d1c64838a03a220df4c5578c892c5819c5
Tommi Helineva <tommi.helineva@gmail.com>
2011-10-30 10:29:07 +0200

    Versionumero muutettu

--- src/kirjanpito/ui/Kirjanpito.java
+++ src/kirjanpito/ui/Kirjanpito.java
@@ -28,7 +28,7 @@ public class Kirjanpito implements Runnable {
 	private File configFile;
 	
 	public static final String APP_NAME = "Tilitin";
-	public static final String APP_VERSION = "1.1.0";
+	public static final String APP_VERSION = "1.1.1";
 	public static final String LOGGER_NAME = "kirjanpito";
 	
 	private Kirjanpito() {
