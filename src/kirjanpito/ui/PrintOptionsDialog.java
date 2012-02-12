e944fe92934cce402bad3afde15dbfbdda3f92b5
Tommi Helineva <tommi.helineva@gmail.com>
2012-02-12 16:22:16 +0200

    Tulosteasetusikkunassa voi valita Page Up- ja Page Down -näppäimillä, muodostetaanko tuloste koko tilikaudelta, yhdeltä kuukaudelta vai muulta aikaväliltä

--- src/kirjanpito/ui/PrintOptionsDialog.java
+++ src/kirjanpito/ui/PrintOptionsDialog.java
@@ -11,17 +11,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -30,6 +33,7 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSeparator;
 import javax.swing.JSpinner;
+import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -38,7 +42,7 @@ import kirjanpito.db.Period;
 
 /**
  * Tulosteen asetusikkuna.
- * 
+ *
  * @author Tommi Helineva
  */
 public class PrintOptionsDialog extends JDialog {
@@ -55,9 +59,9 @@ public class PrintOptionsDialog extends JDialog {
 	private Period period;
 	private Date documentDate;
 	private int result;
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public PrintOptionsDialog(Frame owner, String title) {
 		super(owner, title, true);
 	}
@@ -93,16 +97,16 @@ public class PrintOptionsDialog extends JDialog {
 
 	/**
 	 * Palauttaa alkamispäivämäärän.
-	 * 
+	 *
 	 * @return alkamispäivämäärä
 	 */
 	public Date getStartDate() {
 		return startDate;
 	}
-	
+
 	/**
 	 * Asettaa alkamispäivämäärän.
-	 * 
+	 *
 	 * @param startDate alkamispäivämäärä
 	 */
 	public void setStartDate(Date startDate) {
@@ -111,7 +115,7 @@ public class PrintOptionsDialog extends JDialog {
 
 	/**
 	 * Palauttaa päättymispäivämäärän.
-	 * 
+	 *
 	 * @return päättymispäivämäärä
 	 */
 	public Date getEndDate() {
@@ -120,7 +124,7 @@ public class PrintOptionsDialog extends JDialog {
 
 	/**
 	 * Asettaa päättymispäivämäärän.
-	 * 
+	 *
 	 * @param endDate päättymispäivämäärä
 	 */
 	public void setEndDate(Date endDate) {
@@ -148,7 +152,7 @@ public class PrintOptionsDialog extends JDialog {
 	public int getResult() {
 		return result;
 	}
-	
+
 	/**
 	 * Luo ikkunan komponentit.
 	 */
@@ -160,7 +164,7 @@ public class PrintOptionsDialog extends JDialog {
 				result = JOptionPane.CANCEL_OPTION;
 			}
 		});
-		
+
 		createToggleButtonPanel();
 		createButtonPanel();
 		contentPanel = new JPanel(new GridBagLayout());
@@ -171,22 +175,31 @@ public class PrintOptionsDialog extends JDialog {
 		contentPanel.add(createDateSelectionPanel(), c);
 		addExtraOptions(contentPanel);
 
+		rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_UP, 0), "prevPeriod");
+		rootPane.getActionMap().put("prevPeriod", prevPeriodListener);
+
+		rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_DOWN, 0), "nextPeriod");
+		rootPane.getActionMap().put("nextPeriod", nextPeriodListener);
+
 		pack();
 		setLocationRelativeTo(getOwner());
+		startDateTextField.requestFocusInWindow();
 	}
-	
+
 	private void createToggleButtonPanel() {
 		JPanel panel = new JPanel();
 		panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		BoxLayout layout = new BoxLayout(panel, BoxLayout.LINE_AXIS);
 		panel.setLayout(layout);
-		
+
 		toggleButtons = new JRadioButton[3];
 		toggleButtons[0] = new JRadioButton("Koko tilikausi");
 		toggleButtons[0].setMnemonic('t');
 		toggleButtons[0].setSelected(true);
 		toggleButtons[0].addActionListener(periodActionListener);
-		
+
 		toggleButtons[1] = new JRadioButton("Kuukausi");
 		toggleButtons[1].setMnemonic('K');
 		toggleButtons[1].addActionListener(monthActionListener);
@@ -206,7 +219,7 @@ public class PrintOptionsDialog extends JDialog {
 		panel.add(toggleButtons[2]);
 		add(panel, BorderLayout.NORTH);
 	}
-	
+
 	private JPanel createDateSelectionPanel() {
 		GridBagConstraints c1 = new GridBagConstraints();
 		GridBagConstraints c2 = new GridBagConstraints();
@@ -229,26 +242,26 @@ public class PrintOptionsDialog extends JDialog {
 		endDateSpinner.setEditor(endDateTextField);
 
 		JLabel label;
-		
+
 		label = new JLabel("Alkamispäivämäärä");
 		label.setDisplayedMnemonic('A');
 		label.setLabelFor(startDateTextField);
 		c1.anchor = GridBagConstraints.WEST;
 		c1.insets = new Insets(10, 10, 10, 5);
 		panel.add(label, c1);
-		
+
 		c2.fill = GridBagConstraints.HORIZONTAL;
 		c2.insets = new Insets(10, 5, 10, 10);
 		c2.weightx = 1.0;
 		panel.add(startDateSpinner, c2);
-		
+
 		label = new JLabel("Päättymispäivämäärä");
 		label.setDisplayedMnemonic('P');
 		label.setLabelFor(endDateTextField);
 		c1.gridx = 0;
 		c1.gridy = 1;
 		panel.add(label, c1);
-		
+
 		c2.gridx = c2.gridy = 1;
 		c2.insets = new Insets(10, 5, 10, 10);
 		panel.add(endDateSpinner, c2);
@@ -261,11 +274,11 @@ public class PrintOptionsDialog extends JDialog {
 		panel.add(new JSeparator(SwingConstants.HORIZONTAL), c1);
 		return panel;
 	}
-	
+
 	private void createButtonPanel() {
 		GridBagConstraints c = new GridBagConstraints();
 		JPanel panel = new JPanel(new GridBagLayout());
-		
+
 		okButton = new JButton("OK");
 		okButton.setPreferredSize(new Dimension(100, 30));
 		okButton.addActionListener(new ActionListener() {
@@ -288,7 +301,7 @@ public class PrintOptionsDialog extends JDialog {
 		c.insets = new Insets(5, 10, 10, 5);
 		c.weightx = 1.0;
 		panel.add(cancelButton, c);
-		
+
 		c.gridx = 2;
 		c.insets = new Insets(5, 5, 10, 10);
 		c.weightx = 0.0;
@@ -296,14 +309,14 @@ public class PrintOptionsDialog extends JDialog {
 		add(panel, BorderLayout.SOUTH);
 		rootPane.setDefaultButton(okButton);
 	}
-	
+
 	protected Dimension getFrameMinimumSize() {
 		return new Dimension(450, 200);
 	}
-	
+
 	protected void addExtraOptions(JPanel panel) {
 	}
-	
+
 	public void accept() {
 		try {
 			startDate = startDateTextField.getDate();
@@ -362,6 +375,39 @@ public class PrintOptionsDialog extends JDialog {
 			startDateTextField.setDate(cal.getTime());
 			cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
 			endDateTextField.setDate(cal.getTime());
+		}
+	};
+
+	private AbstractAction prevPeriodListener = new AbstractAction() {
+		private static final long serialVersionUID = 1L;
+
+		public void actionPerformed(ActionEvent e) {
+			if (toggleButtons[1].isSelected()) {
+				toggleButtons[0].setSelected(true);
+				periodActionListener.actionPerformed(null);
+			}
+			else if (toggleButtons[2].isSelected()) {
+				toggleButtons[1].setSelected(true);
+				monthActionListener.actionPerformed(null);
+			}
+
+			startDateTextField.requestFocusInWindow();
+		}
+	};
+
+	private AbstractAction nextPeriodListener = new AbstractAction() {
+		private static final long serialVersionUID = 1L;
+
+		public void actionPerformed(ActionEvent e) {
+			if (toggleButtons[0].isSelected()) {
+				toggleButtons[1].setSelected(true);
+				monthActionListener.actionPerformed(null);
+			}
+			else if (toggleButtons[1].isSelected()) {
+				toggleButtons[2].setSelected(true);
+			}
+
+			startDateTextField.requestFocusInWindow();
 		}
 	};
 
