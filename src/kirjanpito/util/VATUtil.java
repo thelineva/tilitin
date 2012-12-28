42c897a0a9d9cfb7fcec2b44e7010b0915c8a74a
Tommi Helineva <tommi.helineva@gmail.com>
2012-12-28 18:48:29 +0200

    Korjaus arvonlisäveron laskemiseen

--- src/kirjanpito/util/VATUtil.java
+++ src/kirjanpito/util/VATUtil.java
@@ -6,13 +6,13 @@ import java.math.RoundingMode;
 public class VATUtil {
 	private static final BigDecimal HUNDRED = new BigDecimal("100");
 
-	public static BigDecimal addVatAmount(BigDecimal percent, BigDecimal amount) {
-		return amount.multiply(percent.divide(percent));
+	public static BigDecimal addVatAmount(BigDecimal percentage, BigDecimal amount) {
+		return amount.multiply(percentage.divide(HUNDRED));
 	}
 
-	public static BigDecimal subtractVatAmount(BigDecimal percent, BigDecimal amount) {
+	public static BigDecimal subtractVatAmount(BigDecimal percentage, BigDecimal amount) {
 		BigDecimal f = BigDecimal.ONE.subtract(BigDecimal.ONE.divide(
-				percent.add(HUNDRED).divide(HUNDRED), 14, RoundingMode.HALF_UP));
+				percentage.add(HUNDRED).divide(HUNDRED), 14, RoundingMode.HALF_UP));
 		return amount.multiply(f).setScale(2, RoundingMode.HALF_UP);
 	}
 }
